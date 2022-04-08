464766a7fbe5b20f3123af9b6febe78294fd1321
Alexander <Ciga_fu80@student.itstep.org>
2022-04-08 12:44:49 +0300

    Add worker variations

---
+++ ConsoleApp1/Classes/Cooker.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp1.Interfaces;
+using System;
+
+namespace ConsoleApp1.Classes
+{
+    internal class Cooker : IWorker
+    {
+        public IShift Shift { get; set; }
+        public  string Name { get; set; }
+        public float HourlyRate { get; set; }
+        public Cooker() : this("", new MorningShift(), 50.5f)
+        {
+        }
+        public Cooker(string name, IShift shift, float hourlyRate)
+        {
+            Name = name;
+            Shift = shift;
+            HourlyRate = hourlyRate;
+        }
+        public float CalculateSalary()
+        {
+            float salary = 0;
+            salary = HourlyRate * Shift.CalculateWorkHours();
+            return salary;
+        }
+    }
+}
---
+++ ConsoleApp1/Classes/SeniorCooker.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleApp1.Interfaces;
+using System;
+
+
+namespace ConsoleApp1.Classes
+{
+    internal class SeniorCooker : IWorker
+    {
+        public IShift Shift { get; set; }
+        public string Name { get; set; }
+        public float HourlyRate { get; set; }
+        public SeniorCooker() : this("", new MorningShift(), 100.5f)
+        {
+        }
+        public SeniorCooker(string name, IShift shift, float hourlyRate)
+        {
+            Name = name;
+            Shift = shift;
+            HourlyRate = hourlyRate;
+        }
+        public float CalculateSalary()
+        {
+            float salary = 0;
+            salary = HourlyRate * Shift.CalculateWorkHours();
+            return salary;
+        }
+    }
+}
