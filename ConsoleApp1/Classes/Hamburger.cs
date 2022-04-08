5c5b4ba074fb7993b372770b12898a28a86eb874
Alexander <Ciga_fu80@student.itstep.org>
2022-04-08 11:22:09 +0300

    Add food classes

---
+++ ConsoleApp1/Classes/Hamburger.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleApp1.Interfaces;
+
+namespace ConsoleApp1.Classes
+{
+    internal class Hamburger : IFood
+    {
+        public void Cook()
+        {
+            System.Console.WriteLine("You cook hamburgers");
+        }
+    }
+}
---
+++ ConsoleApp1/Classes/IceCream.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleApp1.Interfaces;
+
+namespace ConsoleApp1.Classes
+{
+    internal class IceCream : IFood
+    {
+        public void Cook()
+        {
+            System.Console.WriteLine("You cook a ice cream");
+        }
+    }
+}
